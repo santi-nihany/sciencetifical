6944dda9573467b21c67b67d95a3a3f0beeb0b80
Luz Alba Posse <luzalbaposse@gmail.com>
2023-08-17 04:52:01 -0300

    trying to do smth different a ver si funciona

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,14 +10,14 @@ export const authOptions: NextAuthOptions = {
       name: "Worldcoin",
       type: "oauth",
       wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
-      authorization: { params: { scope: "openid" } },
+     // authorization: { params: { scope: "openid" } },
       clientId: process.env.WLD_CLIENT_ID,
       clientSecret: process.env.WLD_CLIENT_SECRET,
       idToken: true,
       profile(profile) {
         return {
           id: profile.sub,
-          name: profile.sub,
+          //name: profile.sub,
           credentialType: profile["https://id.worldcoin.org/beta"].credential_type,
         }
       },
